f9cbc771d922a0e43297cf9552519f91d293c7fa
johns <jsantore@bridgew.edu>
2023-10-10 13:34:43 -0400

    Initial commit

---
+++ AnimationFromSheet.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"image"
+	"log"
+	"path"
+)
+
+//go:embed assets/*
+var EmbeddedAssets embed.FS
+
+const (
+	WINDOW_WIDTH   = 1000
+	WINDOW_HEIGHT  = 1000
+	COIN_DIMENSION = 512.0
+	FRAME_COUNT    = 4
+)
+
+type AnimatedSpriteDemo struct {
+	CoinImage  *ebiten.Image
+	xFrame     int
+	yFrame     int
+	FrameDelay int
+}
+
+func (demo *AnimatedSpriteDemo) Update() error {
+	demo.FrameDelay += 1
+	if demo.FrameDelay%5 == 0 {
+		demo.xFrame += 1
+		if demo.xFrame >= FRAME_COUNT {
+			demo.xFrame = 0
+			demo.yFrame += 1
+			if demo.yFrame >= FRAME_COUNT {
+				demo.yFrame = 0
+			}
+		}
+	}
+	return nil
+}
+
+// Draw Heavily based on the official ebiten animation demo
+// https://ebitengine.org/en/examples/animation.html#Code
+func (demo *AnimatedSpriteDemo) Draw(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(COIN_DIMENSION/2, COIN_DIMENSION/2)
+	frameX := demo.xFrame * COIN_DIMENSION
+	frameY := demo.yFrame * COIN_DIMENSION
+	screen.DrawImage(demo.CoinImage.SubImage(image.Rect(frameX, frameY,
+		frameX+COIN_DIMENSION, frameY+COIN_DIMENSION)).(*ebiten.Image), op)
+}
+
+func (demo AnimatedSpriteDemo) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return outsideWidth, outsideHeight
+}
+
+func main() {
+	coin := LoadEmbeddedImage("", "Coin_Spin_Animation_A.png")
+	demo := AnimatedSpriteDemo{CoinImage: coin} //xFrame and yFrame deliberately 0
+	ebiten.SetWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT)
+	ebiten.SetWindowTitle("Sprite animation on Sprite Sheet")
+	err := ebiten.RunGame(&demo)
+	if err != nil {
+		fmt.Println("Error running game:", err)
+	}
+}
+
+func LoadEmbeddedImage(folderName string, imageName string) *ebiten.Image {
+	embeddedFile, err := EmbeddedAssets.Open(path.Join("assets", folderName, imageName))
+	if err != nil {
+		log.Fatal("failed to load embedded image ", imageName, err)
+	}
+	ebitenImage, _, err := ebitenutil.NewImageFromReader(embeddedFile)
+	if err != nil {
+		fmt.Println("Error loading tile image:", imageName, err)
+	}
+	return ebitenImage
+}
