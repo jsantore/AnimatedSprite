8eab9c5586cb63e8c2a4c0eae895016eb405ee77
shinjitsu <shinjitsu@auxilioabalto.me>
2025-10-16 09:29:41 -0400

    prep for day class

--- nightDemo1.go
+++ nightDemo1.go
@@ -42,15 +42,12 @@ func main() {
 	}
 }
 func (demo *AnimatedSpriteDemo) Update() error {
-	demo.FrameDelay += 1
-	if demo.FrameDelay%5 == 0 {
-		demo.xFrame += 1
-		if demo.xFrame >= FRAME_COUNT {
-			demo.xFrame = 0
-			demo.yFrame += 1
-			if demo.yFrame >= FRAME_COUNT {
-				demo.yFrame = 0
-			}
+	demo.xFrame += 1
+	if demo.xFrame >= FRAME_COUNT {
+		demo.xFrame = 0
+		demo.yFrame += 1
+		if demo.yFrame >= FRAME_COUNT {
+			demo.yFrame = 0
 		}
 	}
 	return nil
